29decb7460e9fc868f04a4d64c8f9924a459f1f4
Gabriel Rodriguez <rodriel.ant@gmail.com>
2018-08-19 19:10:23 -0500

    now displays if todolist is empty

--- uglytodolist.js
+++ uglytodolist.js
@@ -1,15 +1,18 @@
 const todoList = {
   todos: [],
   displayTodos: function() {
-    console.log('My Todos:');
-    for (var i = 0; i < this.todos.length; i++) {
-    console.log(this.todos[i].todoText);
-    if (this.todos[i].completed === false) {
-     console.log('Incomplete');
-   } else {
-     console.log('Complete!'); }
+    if (this.todos.length === 0) {
+      console.log("Your todolist is empty.");
+    } else {
+      console.log('My Todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+      console.log(this.todos[i].todoText);
+      if (this.todos[i].completed === false) {
+       console.log('Incomplete');
+     } else {
+       console.log('Complete!'); }
     }
-
+    }
   },
 
   addTodo: function(todoText) {
